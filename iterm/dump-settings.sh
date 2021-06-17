1c2001b0f595c1d5ad5c3de947fa283b99375cf1
antoine jubin <antoine.jubin@depinfonancy.net>
2021-06-17 12:28:37 +0200

    add dotfiles for iterm

---
+++ iterm/dump-settings.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cp ~/Library/Preferences/com.googlecode.iterm2.plist ~/dotfiles/iterm
+echo "Preferences synchronized, don't forget to commit and push the file"
+echo "to apply these changes, open iterm settings and follow http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/"
