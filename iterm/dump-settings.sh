62a25dc49abde3f7b5cb4dc55ce2c2da3ffc7107
antoine jubin <antoine.jubin@depinfonancy.net>
2021-06-24 12:25:17 +0200

    feat(iterm): sync settings

--- iterm/dump-settings.sh
+++ iterm/dump-settings.sh
@@ -2,3 +2,9 @@
 cp ~/Library/Preferences/com.googlecode.iterm2.plist ~/dotfiles/iterm
 echo "Preferences synchronized, don't forget to commit and push the file"
 echo "to apply these changes, open iterm settings and follow http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/"
+
+# TODO from here http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+# Specify the preferences directory
+#defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+#defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
