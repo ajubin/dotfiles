2e12f2a237ab04bfbf8abede02ce27001ec207bf
ajubin <antoine.jubin@gmail.com>
2022-10-07 17:04:55 +0200

    format script

--- scripts/useful.sh
+++ scripts/useful.sh
@@ -1,19 +1,19 @@
 # Chechout a git branch
-fbr(){
- gwip
- git --no-pager branch --format='%(refname:short)' | fzf |  awk '{print $1}' | xargs -J {} git checkout {}
- gunwip
+fbr() {
+  gwip
+  git --no-pager branch --format='%(refname:short)' | fzf | awk '{print $1}' | xargs -J {} git checkout {}
+  gunwip
 
 }
 # Delete local git branches (multiselect possible)
-fdbr(){
- git --no-pager branch --format='%(refname:short)' | fzf -m | awk '{print $1}' | xargs -J {} git branch -D {}
+fdbr() {
+  git --no-pager branch --format='%(refname:short)' | fzf -m | awk '{print $1}' | xargs -J {} git branch -D {}
 }
 
 # browse chrome history
-chromeHistory(){
+chromeHistory() {
   local cols sep google_history open
-  cols=$(( COLUMNS / 3 ))
+  cols=$((COLUMNS / 3))
   sep='{::}'
 
   if [ "$(uname)" = "Darwin" ]; then
@@ -27,19 +27,18 @@ chromeHistory(){
   sqlite3 -separator $sep /tmp/h \
     "select substr(title, 1, $cols), url
      from urls order by last_visit_time desc" |
-  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+    awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+    fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open >/dev/null 2>/dev/null
 }
 
 # Execute local scripts
-yarns(){
- cat package.json | jq -r '.scripts' | sed -e 's/  "\(.*\)":\s*\(.*\)$/\1|\2/' | sed -e '1d;$d' | column -t -c 2 -s '|' | fzf --reverse -n 1 --height=80%| awk '{print $1}' | xargs yarn
+yarns() {
+  cat package.json | jq -r '.scripts' | sed -e 's/  "\(.*\)":\s*\(.*\)$/\1|\2/' | sed -e '1d;$d' | column -t -c 2 -s '|' | fzf --reverse -n 1 --height=80% | awk '{print $1}' | xargs yarn
 }
 
-fjest(){
+fjest() {
   find . -path './src/**/*.test.js' | fzf | xargs yarn jest
 }
-
 
 # Create new up to date branch
 gnb() {
@@ -57,9 +56,9 @@ alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|d
 snap() {
   # diskSize=`find $1/**/*.snap -type f -exec du -ch {} + | grep total$ | awk '{print $1}'`
   echo $(find $PWD/src/**/*.snap -type f)
-  echo `wc -l \`find $PWD/src/**/*.snap -type f\` | grep total$ | awk '{print $1}'`
-  echo "number of lines: $numberOfLine" 
-  echo "diskSize : $diskSize" 
+  echo $(wc -l $(find $PWD/src/**/*.snap -type f) | grep total$ | awk '{print $1}')
+  echo "number of lines: $numberOfLine"
+  echo "diskSize : $diskSize"
 }
 
 c() {
@@ -68,11 +67,11 @@ c() {
 
 searchgit() {
   STRING_TO_SEARCH=$1
-  shift;
+  shift
 
   git log -S"$STRING_TO_SEARCH" --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --reverse --tiebreak=index \
+    --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --reverse --tiebreak=index \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
@@ -85,24 +84,23 @@ biggestFiles() {
   find $1/** -type f -name '*.ts*' -not -name '*.test.ts*' -exec wc -l {} + | sort -rn | head -n 10
 }
 
-
 cleanupGit() {
   # https://devconnected.com/how-to-clean-up-git-branches/#Clean_Up_Local_Git_Branches
   git fetch --quiet --prune
-  git checkout --quiet master
+  git checkout --quiet staging
   # Make sure git status and others stay fast
   git prune
   git gc
   # Delete fully merged branches
-  git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d
+  git branch --merged | egrep -v "(^\*|master|staging|dev)" | xargs git branch -d
   # Delete squash-and-merged branches
   # https://github.com/not-an-aardvark/git-delete-squashed
   git for-each-ref refs/heads/ "--format=%(refname:short)" |
-  while read branch; do
-    mergeBase=$(git merge-base master $branch) &&
-    [[ $(git cherry master $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] &&
-    git branch -D $branch
-  done
+    while read branch; do
+      mergeBase=$(git merge-base staging $branch) &&
+        [[ $(git cherry staging $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] &&
+        git branch -D $branch
+    done
   # Delete branches that were deleted on origin
   git branch -vv | grep 'origin/.*: disparue]' | awk '{print $1}' | xargs git branch -D
   echo "🗑  Deleted outdated branches. To recover one of the deleted branches do this:
